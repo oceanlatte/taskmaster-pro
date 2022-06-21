bda5e0d537d594830cd218a2666d22fbecabd11a
ocean <osciris.veliz@gmail.com>
2022-06-21 16:28:43 -0500

    add date editing textbox and focus

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,7 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// edit task on click
 $(".list-group").on("click", "p", function() {
   var text = $(this)
     .text()
@@ -58,6 +59,7 @@ $(".list-group").on("click", "p", function() {
     textInput.trigger("focus");
 });
 
+// add blur to textbox on click
 $(".list-group").on("blur", "textarea", function() {
   // get the textarea's current value/text
   var text = $(this)
@@ -85,6 +87,57 @@ $(".list-group").on("blur", "textarea", function() {
 
   //replace textarea with p elment
   $(this).replaceWith(taskP);
+});
+
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  //get current text
+  var date = $(this)
+  .text()
+  .trim();
+
+  // create new input element 
+  var dateInput = $("<input>")
+  .attr("type", "text")
+  .addClass("form-control")
+  .val(date);
+
+  //swap out elements
+  $(this).replaceWith(dateInput);
+
+  //automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+//value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  //get current text
+  var date = $(this)
+  .val()
+  .trim();
+
+  // get the parent ul's id attribue
+  var status =$(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  // get the task's position in the list of the other li elements
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  // update task in array and re-save to localStorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+  .addClass("badge badge-primary badge-pill")
+  .text(date);
+
+  //replace input with span element
+  $(this).replaceWith(taskSpan);
 });
 
 
