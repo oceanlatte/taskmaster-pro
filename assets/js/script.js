463b136a7a07425b1f623b3898e290ecc5c284ab
ocean <osciris.veliz@gmail.com>
2022-06-24 21:18:40 -0500

    add array to save updates when tasks change

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,6 +140,57 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
+// jQuery Selector for all list-group elements
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone:",
+  activate: function(event) {
+    console.log("active", this);
+  },
+  deactivate: function(event) {
+    console.log("deactive", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task date in
+    var tempArr = [];
+
+    // loop over current set of children in the sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim()
+
+      // add task data to the temporary array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    console.log(tempArr);
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
